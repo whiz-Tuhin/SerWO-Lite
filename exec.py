c0b5a510d0e49d7adc30cb62cca0d141b803def5
tuhinkhare <tuhinkhare3@gmail.com>
2023-07-21 09:16:26 +0530

    Fixed Bugs. Restructured project

--- exec.py
+++
@@ -1,4 +0,0 @@
-def user_function():
-    return 1+2
-if __name__ == "__main__":
-    user_function
--- orchestrator/app/views.py
+++ orchestrator/app/views.py
@@ -6,14 +6,23 @@ from python.src.utils.classes.commons.serwo_objects import build_serwo_list_obje
 from copy import deepcopy
 
 import json
+import sys
 import networkx as nx
-import importlib
+import importlib.util
 
 node_output_map = dict()
 
 # routine to execute a user function python file
 def execute_from_module(basepath, modulepath, nodename, request_input):
-    module = importlib.import_module(f"{basepath}/{modulepath}")
+
+    print(basepath, modulepath, nodename, request_input)
+    spec = importlib.util.spec_from_file_location("user_function", f"{basepath}/{modulepath}")
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    # mod.user_function()
+
+
+    # module = importlib.import_module(f"/Users/tuhinkhare/Work/IISc/DREAM-Lab/serwo-project/serwo-lite/SerWO-Lite/orchestrator/{basepath}/{modulepath}")
     print(f"Calling function {nodename}")
 
     if isinstance(request_input, list):
@@ -34,7 +43,9 @@ def execute_from_module(basepath, modulepath, nodename, request_input):
 
     statusCode = 200
     try:
-        response = module.user_function(serwo_object)
+        # response = module.user_function(serwo_object)
+        response = mod.user_function(serwo_object) 
+        print("response here", response.to_json())
     except Exception as e:
         print(e)
         statusCode = 500
@@ -46,7 +57,7 @@ def execute_from_module(basepath, modulepath, nodename, request_input):
     return dict(
         statusCode=statusCode,
         body=response.get_body(),
-        metadata=response.metadata()
+        metadata=response.get_metadata()
     )
 
 
@@ -55,8 +66,8 @@ def execute(request):
     if request.method == 'POST':
         try:
             req_body = json.loads(request.body)
+            print(req_body)
             dag_description_path = req_body["path"]
-            
             # populate the dict excluding path as a blacklist key
             data = dict()
             for k,v in req_body.items():
@@ -69,18 +80,19 @@ def execute(request):
             top_sort_nodes = list(nx.topological_sort(serwo_nx_dag))
         
             # execute based on the top sort nodes
+            last_response = None
             for v in top_sort_nodes:
                 if serwo_nx_dag.in_degree(v) == 0:
-                    out = execute_from_module(dag_description_path, serwo_nx_dag[v]["Path"], serwo_nx_dag[v]["NodeName"], req_body)
+                    out = execute_from_module(serwo_nx_dag.nodes[v]["Path"], serwo_nx_dag.nodes[v]["EntryPoint"], serwo_nx_dag.nodes[v]["NodeName"], req_body)
                 elif serwo_nx_dag.in_degree(v) == 1:
-                    v_pred = serwo_nx_dag.predecessors(v)[0]
+                    v_pred = list(serwo_nx_dag.predecessors(v))[0]
                     v_input = node_output_map[v_pred]
-                    out = execute_from_module(dag_description_path , serwo_nx_dag[v]["Path"], serwo_nx_dag[v]["NodeName"], v_input)
+                    out = execute_from_module(serwo_nx_dag.nodes[v]["Path"] , serwo_nx_dag.nodes[v]["EntryPoint"], serwo_nx_dag.nodes[v]["NodeName"], v_input)
                 else:
                     v_input_list = []
                     for v_pred in serwo_nx_dag.predecessors(v):
                         v_input_list.append(node_output_map[v_pred])
-                    out = execute_from_module(dag_description_path , serwo_nx_dag[v]["Path"], serwo_nx_dag[v]["NodeName"], v_input_list)
+                    out = execute_from_module(serwo_nx_dag.nodes[v]["Path"], serwo_nx_dag.nodes[v]["EntryPoint"], serwo_nx_dag.nodes[v]["NodeName"], v_input_list)
                 
                 # return a response in case of failure
                 if out["statusCode"] == 500:
@@ -88,6 +100,7 @@ def execute(request):
                 
                 # store in map
                 node_output_map[v] = out
+                last_response = out
 
                 print("Printing node map here")
                 print(node_output_map)
@@ -95,7 +108,7 @@ def execute(request):
             # Process the JSON payload here
             # For example, you can access the data using data['key_name']
             # Replace 'key_name' with the appropriate key present in your JSON payload
-            response_data = {'message': 'Success', 'data': data}
+            response_data = {'message': 'Success', 'data': last_response}
             return JsonResponse(response_data, status=200)
         except json.JSONDecodeError:
             return JsonResponse({'message': 'Invalid JSON payload'}, status=400)
